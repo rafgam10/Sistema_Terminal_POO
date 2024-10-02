bb88211dd471c82daddf7d49b0a7be682b995980
rafgam10 <rafgam19@gmail.com>
2024-10-02 09:12:36 -0300

    Implementação das Classes de Cadastro com o Login.

--- login.py
+++ login.py
@@ -1,9 +1,13 @@
+from cadastro import *
+
 class Login:
 
     #Construtor:
     def __init__(self, codigo:str, senha:str):
         self.codigo=codigo
         self.senha=senha
+        self.listaUsuario = []
+        self.listaProduto = []
 
     #Método de Verificação:
     def VerificarLogin(self, codigoInput:str, senhaInput:str):
@@ -13,7 +17,33 @@ class Login:
             print("Acesso negado.")
             print("Tente logan novamnte.")
 
-    
+    #Método de Cadastro de Usuário:
+    def CadastroUsuario(self, codigoInput, nomeInput, senhaInput):
+        status = False
+        for i in self.listaUsuario:
+            if self.listaUsuario.codigo != codigoInput:
+                status = True
+            else:
+                status = False
+
+        if status:
+            var=Usuario(codigoInput, nomeInput, senhaInput)
+            self.listaUsuario.append(var)
+            print(f"Usuário Criando com Sucesso!")
+
+    #Método de Cadastro de Produto:
+    def CadastroProduto(self, codigoInput, nomeInput, categoriaInput, quantidadeInput, precoInput):
+        status = False
+        for i in self.listaProduto:
+            if self.listaProduto.codigo != codigoInput:
+                status = True
+            else:
+                status = False
+
+        if status:
+            var=Produto(codigoInput, nomeInput, categoriaInput, quantidadeInput, precoInput)
+            self.listaProduto.append(var)
+            print(f"Produto Criando com Sucesso!")
 
     
 
